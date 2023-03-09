0b28fc768d041383f82fbb88693722baeec3da06
Jan <jan.krol.legowski@gmail.com>
2023-03-09 17:42:49 +0100

    Set number of guests add asserts -checking names of searched hotels.

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
@@ -18,9 +22,29 @@ public class HotelSearch {
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-        driver.findElement(By.name("checkin")).sendKeys("17/04/2021");
-        driver.findElement(By.name("checkout")).sendKeys("20/04/2021");
+        driver.findElement(By.name("checkin")).sendKeys("17/03/2023");
+        //driver.findElement(By.name("checkout")).sendKeys("1/04/2023");
+        driver.findElement(By.name("checkout")).click();
+        //driver.findElement(By.xpath("//td[@class='day ' and text()='30']")).click(); // ty akurat dziala to rozwiazanie ale moze sie wyswietlac wiecej 30-tek w kalendarz, zalezy do miesiaca
 
+        //lepsze rozwiazanie
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']")).stream().filter(WebElement::isDisplayed)
+        .findFirst()
+        .ifPresent(WebElement::click);
+
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b"))
+                .stream()
+                .map(el->el.getAttribute("textContent"))//wczesniej uzywalismy el.getText() - ale nie zdazyly wczytac sie wszystkie hotele, wiec z atrybutu.
+                .collect(Collectors.toList());
+
+        Assert.assertEquals("Jumeirah Beach Hotel",hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower",hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana",hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth",hotelNames.get(3));
 
     }
 }
